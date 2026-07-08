df244018d6677d65a378cd480bf191e8c53292cf
Igor Mykoliv <myeduc45@gmail.com>
2026-07-08 23:33:30 +0000

    add repositories name

--- models/github_analytics_dbt/marts/analytics/fct_github_commit_activity.sql
+++ models/github_analytics_dbt/marts/analytics/fct_github_commit_activity.sql
@@ -1,22 +1,27 @@
 select
-    repository_id,
-    author_name,
-    author_email,
-    commit_date,
-    commit_year,
-    commit_month,
-    year_month,
-    weekday_number,
+    c.repository_id,
+    r.repository_name,
+    c.author_name,
+    c.author_email,
+    c.commit_date,
+    c.commit_year,
+    c.commit_month,
+    c.year_month,
+    c.weekday_number,
     count(*) as commit_cnt
 
-from {{ ref('int_github__commit_activity') }}
+from {{ ref('int_github__commit_activity') }} c
+
+left join {{ ref('dim_github__repositories') }} r
+    on c.repository_id = r.repository_id
 
 group by
-    repository_id,
-    author_name,
-    author_email,
-    commit_date,
-    commit_year,
-    commit_month,
-    year_month,
-    weekday_number
+    c.repository_id,
+    r.repository_name,
+    c.author_name,
+    c.author_email,
+    c.commit_date,
+    c.commit_year,
+    c.commit_month,
+    c.year_month,
+    c.weekday_number
